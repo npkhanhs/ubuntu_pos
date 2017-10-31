78ba118c8324fb20849fb31e1ca64c769d642841
HoangHai <hofanghari@gmail.com>
2017-10-31 17:38:08 +0700

    lam gon code

--- pos/StartP.py
+++ pos/StartP.py
@@ -8,30 +8,31 @@ import os
 def change_hostname():
     CN = input("nhap CN: ")
 
-    file_hostname_open = open('/etc/hostname', 'w')
-    file_hostname_open.write(CN)
-    file_hostname_open.close()
+    with open('/etc/hostname', 'w') as file_hostname:
+        file_hostname.write(CN)
 
-    file_hosts_open = open('/etc/hosts', 'r+')
-    file_hosts = file_hosts_open.read()
-    list_file_hosts = file_hosts.split('\n')
-    list_file_hosts[1] = '127.0.0.1\t' + CN
-    file_hosts = '\n'.join(list_file_hosts)
-    file_hosts_open.seek(0)
-    file_hosts_open.write(file_hosts)
-    file_hosts_open.close()
+    with open('/etc/hosts', 'r+') as file_hosts:
+        list_file_hosts = file_hosts.read().split('\n')
+        file_hosts.seek(0)
+        list_file_hosts[1] = '127.0.0.1\t' + CN
+        file_hosts.write('\n'.join(list_file_hosts))
 
-    file_info = open('/etc/pos/info', 'w')
-    file_info.write(CN + '\n')
-    file_info.seek(1)
-    file_info.close()
+    return CN
+
+
+def get_info(CN):
+    with open('/etc/pos/info', 'w') as file_info:
+        file_info.write(CN + '\n')
 
     os.system('dmidecode -s system-serial-number >> info')
     os.system('dmidecode -s system-product-name >> info')
 
+    return '/etc/pos/info'
+
 
 def main():
-    change_hostname()
+    hostname = change_hostname()
+    get_info(hostname)
 
 
 if __name__ == '__main__':

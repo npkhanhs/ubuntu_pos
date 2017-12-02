54afe4986ab992b073c191081714d7e5963b5c98
npkhanhs <npkhanhs@msn.com>
2017-12-02 11:54:21 +0700

    update file

--- pos/StartP.py
+++ pos/StartP.py
@@ -21,18 +21,42 @@ def change_hostname():
 
 
 def get_info(CN):
-    with open('/etc/pos/info', 'w') as file_info:
-        file_info.write(CN + '\n')
+    # with open('/etc/pos/info', 'w') as file_info:
+     #   file_info.write(CN + '\n')
+    file_info = open('/etc/pos/info', 'w')
+    file_info.write(CN + '\n')
+    file_info.close()
 
-    os.system('dmidecode -s system-serial-number >> info')
-    os.system('dmidecode -s system-product-name >> info')
+    os.system('dmidecode -s system-serial-number >> /etc/pos/info')
+    os.system('dmidecode -s system-product-name >> /etc/pos/info')
 
     return '/etc/pos/info'
 
 
+def cconfig_anydesk():
+    # clean config anydesk
+    os.system('rm -rf /home/fepos/.anydesk/*')
+    os.system('rm -rf /home/ccpos/.anydesk/*')
+
+
+def make_swap():
+    if os.path.isfile('/swapfile'):
+        print('swapfile Exist')
+    else:
+        os.system('dd if=/dev/zero of=/swapfile bs=1M count=4000')
+        os.system('chmod 600 /swapfile')
+        os.system('mkswap /swapfile')
+        os.system('swapon /swapfile')
+        os.system('echo "/swapfile none swap sw 0 0" | sudo tee -a /etc/fstab')
+        print('make swap ok')
+
+
 def main():
+
     hostname = change_hostname()
     get_info(hostname)
+    cconfig_anydesk()
+    make_swap()
 
 
 if __name__ == '__main__':

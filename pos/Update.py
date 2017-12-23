cd266cfab29ec88d290d0dd7f0bbc75ce56ec210
npkhanhs <npkhanhs@msn.com>
2017-12-23 16:07:12 +0700

    call nioc

--- pos/Update.py
+++ pos/Update.py
@@ -2,7 +2,7 @@
 # folder download /etc/updatep/git_download
 import os
 
-os.system('cp /etc/updatep/git_download/pos/Update.py /etc/updatep/')
+os.system('cp /etc/updatep/git_download/pos/nioc.py /etc/updatep/')
 os.system('/etc/updatep/nioc.py')
 print("nioc ok")
 #os.system('pip3 install -r /etc/updatep/git_download/requirements.txt')

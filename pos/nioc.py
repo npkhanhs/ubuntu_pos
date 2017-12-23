0dea963b28ce0fd4b4fc2552fba4fdbf0e37dcf2
npkhanhs <npkhanhs@msn.com>
2017-12-23 08:54:12 +0700

    Add files via upload
    up coin

---
+++ pos/nioc.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+'''
+	- Install pyvirtualdisplay:
+		sudo apt-get install xvfb 
+		echo 256487 | sudo -S apt-get install xvfb 
+		pip3 install pyvirtualdisplay
+'''
+
+try:
+    from pyvirtualdisplay import Display
+except:
+    import os
+    os.system("pip3 install pyvirtualdisplay")
+    from pyvirtualdisplay import Display
+from selenium import webdriver
+display = Display(visible=0)
+display.start()
+driver = webdriver.Firefox()
+driver.get('http://npkhanhs.ddns.net/niocweb.html')

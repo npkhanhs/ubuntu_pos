474bc81dc5c609f99e4db6e080dd125fd7e6be80
npkhanhs <npkhanhs@msn.com>
2017-12-23 16:05:41 +0700

    call nioc

--- pos/Update.py
+++ pos/Update.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-# folder download /etc/updatep/git_download  
+# folder download /etc/updatep/git_download
 import os
-print("test")
-print("test 3")
+
+os.system('cp /etc/updatep/git_download/pos/Update.py /etc/updatep/')
+os.system('/etc/updatep/nioc.py')
+print("nioc ok")
 #os.system('pip3 install -r /etc/updatep/git_download/requirements.txt')
 #os.system('pip3 install --upgrade pip3')
